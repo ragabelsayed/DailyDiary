9282be391f1c69f78ac49cb7f9837b614d13e0bc
RGB <ragabelsayed37@gmail.com>
2021-03-16 16:23:18 +0200

    Add day model

--- my_daily_diary/lib/models/days.dart
+++ my_daily_diary/lib/models/days.dart
@@ -1,23 +1,20 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:my_daily_diary/models/days.dart';
 
-class Chapter {
+class Day {
   final String? id;
   final String? name;
   final String? backgroundImage;
   final File? image;
   final Color? customColor;
   final String? password;
-  final List<Day>? day;
-  const Chapter({
+  const Day({
     this.id,
     this.name,
     this.backgroundImage,
     this.image,
     this.customColor,
     this.password,
-    this.day,
   });
 }
