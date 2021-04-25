072d01cf5b5996387a6264c43e8c84789fc0abbf
RGB <ragabelsayed37@gmail.com>
2021-04-25 15:08:40 +0200

    make page  model hive model and make pageAdapter file

--- my_daily_diary/lib/models/page.dart
+++ my_daily_diary/lib/models/page.dart
@@ -1,30 +1,57 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 
-class ChapterPage {
+import 'package:hive/hive.dart';
+part 'page.g.dart';
+
+@HiveType(typeId: 2)
+class ChapterPage extends HiveObject {
+  @HiveField(0)
   String id;
+
+  @HiveField(1)
   String name;
+
+  @HiveField(2)
   DateTime date;
+
+  @HiveField(3)
   File? image;
+
+  @HiveField(4)
   Color customColor;
-  // String? password;
+
+  @HiveField(5)
   List<Map<String, String>> writingArea;
+
+  @HiveField(6)
   Color currenTextColor;
+
+  @HiveField(7)
   TextAlign textAlign;
+
+  @HiveField(8)
   bool fontweight;
+
+  @HiveField(9)
   bool fontStyle;
+
+  @HiveField(10)
   String fontName;
+
+  @HiveField(11)
   List<bool> isSelected1;
+
+  @HiveField(12)
   List<bool> isSelected2;
 
+  // String? password;
   ChapterPage({
     required this.id,
     required this.name,
     required this.date,
     this.image,
     required this.customColor,
-    // this.password,
     required this.writingArea,
     required this.currenTextColor,
     required this.textAlign,
@@ -33,6 +60,7 @@ class ChapterPage {
     required this.fontName,
     required this.isSelected1,
     required this.isSelected2,
+    // this.password,
   });
   // String id = DateTime.now().toString();
   // String name;
