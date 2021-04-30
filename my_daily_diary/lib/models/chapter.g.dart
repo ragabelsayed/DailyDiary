eb895aa4e5ae151c120b8f2c08aa2e4622d11068
RGB <ragabelsayed37@gmail.com>
2021-04-30 17:34:04 +0200

    edit ChapterAdapter

--- my_daily_diary/lib/models/chapter.g.dart
+++ my_daily_diary/lib/models/chapter.g.dart
@@ -1,47 +1,44 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'diary.dart';
+part of 'chapter.dart';
 
 // **************************************************************************
 // TypeAdapterGenerator
 // **************************************************************************
 
-class DiaryAdapter extends TypeAdapter<Diary> {
+class ChapterAdapter extends TypeAdapter<Chapter> {
   @override
-  final int typeId = 0;
+  final int typeId = 1;
 
   @override
-  Diary read(BinaryReader reader) {
+  Chapter read(BinaryReader reader) {
     final numOfFields = reader.readByte();
     final fields = <int, dynamic>{
       for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
     };
-    return Diary(
+    return Chapter(
       id: fields[0] as String,
       name: fields[1] as String,
-      image: fields[2] as File?,
-      customColor: fields[3] as Color,
-      chapters: (fields[4] as HiveList).castHiveList(),
-      onClickDiary: fields[5] as bool,
+      image: fields[2] != null ? File(fields[2]) : null,
+      customColor: Color(fields[3]),
+      pages: (fields[4] as HiveList).castHiveList(),
     );
   }
 
   @override
-  void write(BinaryWriter writer, Diary obj) {
+  void write(BinaryWriter writer, Chapter obj) {
     writer
-      ..writeByte(6)
+      ..writeByte(5)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
       ..write(obj.name)
       ..writeByte(2)
-      ..write(obj.image)
+      ..write(obj.image?.path)
       ..writeByte(3)
-      ..write(obj.customColor)
+      ..write(obj.customColor.value)
       ..writeByte(4)
-      ..write(obj.chapters)
-      ..writeByte(5)
-      ..write(obj.onClickDiary);
+      ..write(obj.pages);
   }
 
   @override
@@ -50,7 +47,7 @@ class DiaryAdapter extends TypeAdapter<Diary> {
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      other is DiaryAdapter &&
+      other is ChapterAdapter &&
           runtimeType == other.runtimeType &&
           typeId == other.typeId;
 }
