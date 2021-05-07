528c805d9804f0083026392bb1287f4c5b201fcc
RGB <ragabelsayed37@gmail.com>
2021-05-07 04:17:45 +0200

    Refactor ChapterScreen

--- my_daily_diary/lib/screens/chapter_screen.dart
+++ my_daily_diary/lib/screens/chapter_screen.dart
@@ -4,6 +4,7 @@ import 'package:my_daily_diary/providers/page_data.dart';
 import 'package:my_daily_diary/widgets/change_theme_btn_widget.dart';
 
 import 'package:my_daily_diary/widgets/chapter_screen_widget/Page_view.dart';
+import 'package:my_daily_diary/widgets/chapter_screen_widget/page_list_view.dart';
 
 import 'package:my_daily_diary/widgets/dialog_view.dart';
 
@@ -40,8 +41,6 @@ class _ChapterScreenState extends State<ChapterScreen>
   }
 
   Widget build(BuildContext context) {
-    final _pageData = Provider.of<PageData>(context).items;
-
     //  final routArgs =
     //     ModalRoute.of(context)!.settings.arguments as Map<String, String>;
     // final dayId = routArgs['id'];
@@ -49,7 +48,6 @@ class _ChapterScreenState extends State<ChapterScreen>
     //     .items
     //     .firstWhere((day) => day.id == dayId);
     return Scaffold(
-      appBar: _appBar(),
       body: FutureBuilder(
         future: _getData(),
         builder: (context, snapshot) {
@@ -66,61 +64,14 @@ class _ChapterScreenState extends State<ChapterScreen>
                 Expanded(
                   child: Stack(
                     children: [
-                      // Padding(
-                      //   padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      //   child: ListWheelScrollView(
-                      //     itemExtent: 250,
-                      //     // diameterRatio: 0.1,
-                      //     //magnification: 5.5,
-                      //     // offAxisFraction: 0,
-                      //     // overAndUnderCenterOpacity: 0.8,
-                      //     // perspective: 0.01,
-                      //     // renderChildrenOutsideViewport: false,
-                      //     // squeeze: 5.0,
-                      //     // useMagnifier: false,
-                      //     children: _dayData
-                      //         .map((day) => DaysView(
-                      //               dayData: day,
-                      //               click: () {
-                      //                 print(day.name);
-                      //               },
-                      //             ))
-                      //         .toList(),
-                      //   ),
-                      // ),
-
-                      _pageData.isNotEmpty
-                          ? ListView.builder(
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 5,
-                                vertical: 5,
-                              ),
-                              itemCount: _pageData.length,
-                              scrollDirection: Axis.vertical,
-                              itemBuilder: (context, index) {
-                                final int count = _pageData.length > 10
-                                    ? 10
-                                    : _pageData.length;
-                                final Animation<double> animation =
-                                    Tween<double>(begin: 0.0, end: 1.0).animate(
-                                  CurvedAnimation(
-                                    parent: _animationController,
-                                    curve: Interval((1 / count) * index, 1.0,
-                                        curve: Curves.fastOutSlowIn),
-                                  ),
-                                );
-                                _animationController.forward();
-                                return PagesView(
-                                  pageData: _pageData[index],
-                                  animation: animation,
-                                  animationController: _animationController,
-                                );
-                              },
-                            )
-                          : Center(
-                              child:
-                                  Text('Add Your frist page in this chapter😀'),
-                            ),
+                      CustomScrollView(
+                        slivers: [
+                          _sliverAppBar(),
+                          SliverToBoxAdapter(
+                            child: PageListView(_animationController),
+                          ),
+                        ],
+                      ),
                       Positioned(
                         right: 10,
                         bottom: 10,
@@ -141,32 +92,90 @@ class _ChapterScreenState extends State<ChapterScreen>
                           },
                         ),
                       ),
-                      // Positioned(
-                      //   child: Padding(
-                      //     padding:
-                      //         const EdgeInsets.only(left: 5, top: 5, bottom: 5),
-                      //     child: SizedBox(
-                      //       width: 30,
-                      //       height: 30,
-                      //       child: Material(
-                      //         color: Colors.transparent,
-                      //         child: InkWell(
-                      //           borderRadius: BorderRadius.circular(30),
-                      //           onTap: () {
-                      //             Navigator.pop(context);
-                      //           },
-                      //           child: Icon(
-                      //             Icons.arrow_back,
-                      //             color: Colors.black,
-                      //           ),
-                      //         ),
-                      //       ),
-                      //     ),
-                      //   ),
-                      // ),
                     ],
                   ),
                 ),
+                // Expanded(
+                //   child: Stack(
+                //     children: [
+                //       _pageData.isNotEmpty
+                //           ? ListView.builder(
+                //               padding: const EdgeInsets.symmetric(
+                //                 horizontal: 5,
+                //                 vertical: 5,
+                //               ),
+                //               itemCount: _pageData.length,
+                //               scrollDirection: Axis.vertical,
+                //               itemBuilder: (context, index) {
+                //                 final int count = _pageData.length > 10
+                //                     ? 10
+                //                     : _pageData.length;
+                //                 final Animation<double> animation =
+                //                     Tween<double>(begin: 0.0, end: 1.0).animate(
+                //                   CurvedAnimation(
+                //                     parent: _animationController,
+                //                     curve: Interval((1 / count) * index, 1.0,
+                //                         curve: Curves.fastOutSlowIn),
+                //                   ),
+                //                 );
+                //                 _animationController.forward();
+                //                 return PagesView(
+                //                   pageData: _pageData[index],
+                //                   animation: animation,
+                //                   animationController: _animationController,
+                //                 );
+                //               },
+                //             )
+                //           : Center(
+                //               child:
+                //                   Text('Add Your frist page in this chapter😀'),
+                //             ),
+                //       Positioned(
+                //         right: 10,
+                //         bottom: 10,
+                //         child: FloatingActionButton(
+                //           child: Icon(Icons.add),
+                //           onPressed: () {
+                //             showDialog(
+                //               context: context,
+                //               builder: (context) {
+                //                 return DialogView(
+                //                   name: 'Page Name',
+                //                   hint: 'Ex: 1, 2 ...30 ets',
+                //                   coverName: 'Page Cover',
+                //                   action: AddAction.page,
+                //                 );
+                //               },
+                //             );
+                //           },
+                //         ),
+                //       ),
+                // Positioned(
+                //   child: Padding(
+                //     padding:
+                //         const EdgeInsets.only(left: 5, top: 5, bottom: 5),
+                //     child: SizedBox(
+                //       width: 30,
+                //       height: 30,
+                //       child: Material(
+                //         color: Colors.transparent,
+                //         child: InkWell(
+                //           borderRadius: BorderRadius.circular(30),
+                //           onTap: () {
+                //             Navigator.pop(context);
+                //           },
+                //           child: Icon(
+                //             Icons.arrow_back,
+                //             color: Colors.black,
+                //           ),
+                //         ),
+                //       ),
+                //     ),
+                //   ),
+                // ),
+                //     ],
+                //   ),
+                // ),
                 SizedBox(height: MediaQuery.of(context).padding.bottom),
               ],
             );
@@ -176,15 +185,23 @@ class _ChapterScreenState extends State<ChapterScreen>
     );
   }
 
-  PreferredSizeWidget _appBar() {
-    return AppBar(
+  Widget _sliverAppBar() {
+    return SliverAppBar(
+      floating: true,
+      forceElevated: true,
+      primary: false,
+      // pinned: true,
+      centerTitle: true,
       title: Text(
         'Pages',
         style: Theme.of(context).textTheme.headline6,
       ),
-      centerTitle: true,
+      leading: InkWell(
+        borderRadius: BorderRadius.circular(30),
+        onTap: () => Navigator.of(context).pop(),
+        child: Icon(Icons.arrow_back),
+      ),
       backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
-      // elevation: 0,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(
           bottom: Radius.circular(15),
---
+++ my_daily_diary/lib/widgets/chapter_screen_widget/page_list_view.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:my_daily_diary/providers/page_data.dart';
+import 'package:my_daily_diary/widgets/chapter_screen_widget/page_view.dart';
+import 'package:provider/provider.dart';
+
+class PageListView extends StatelessWidget {
+  final AnimationController _animationController;
+  PageListView(this._animationController);
+  @override
+  Widget build(BuildContext context) {
+    final _pageData = Provider.of<PageData>(context).items;
+
+    return _pageData.isNotEmpty
+        ? ListView.builder(
+            primary: false,
+            shrinkWrap: true,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 5,
+              vertical: 5,
+            ),
+            itemCount: _pageData.length,
+            scrollDirection: Axis.vertical,
+            itemBuilder: (context, index) {
+              final int count = _pageData.length > 10 ? 10 : _pageData.length;
+              final Animation<double> animation =
+                  Tween<double>(begin: 0.0, end: 1.0).animate(
+                CurvedAnimation(
+                  parent: _animationController,
+                  curve: Interval((1 / count) * index, 1.0,
+                      curve: Curves.fastOutSlowIn),
+                ),
+              );
+              _animationController.forward();
+              return PagesView(
+                pageData: _pageData[index],
+                animation: animation,
+                animationController: _animationController,
+              );
+            },
+          )
+        : Center(
+            child: Text('Add Your frist page in this chapter😀'),
+          );
+  }
+  // Padding(
+  //   padding: const EdgeInsets.symmetric(horizontal: 8.0),
+  //   child: ListWheelScrollView(
+  //     itemExtent: 250,
+  // diameterRatio: 0.1,
+  //magnification: 5.5,
+  // offAxisFraction: 0,
+  // overAndUnderCenterOpacity: 0.8,
+  // perspective: 0.01,
+  // renderChildrenOutsideViewport: false,
+  // squeeze: 5.0,
+  // useMagnifier: false,
+  //     children: _dayData
+  //         .map((day) => DaysView(
+  //               dayData: day,
+  //               click: () {
+  //                 print(day.name);
+  //               },
+  //             ))
+  //         .toList(),
+  //   ),
+  // ),
+
+}
