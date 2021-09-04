6c3c1ee8fc5a4cf410dcda63dad96a73e61a0d90
ragab elsayed <ragabelsayed37@gmail.com>
2021-09-04 17:19:53 +0200

    change darkmode switch

--- my_daily_diary/lib/widgets/change_theme_btn_widget.dart
+++ my_daily_diary/lib/widgets/change_theme_btn_widget.dart
@@ -2,16 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../providers/theme.dart';
 
-class ChangeThemeButtonWidget extends StatelessWidget {
+class ChangeThemeButtonWidget extends StatefulWidget {
+  @override
+  _ChangeThemeButtonWidgetState createState() =>
+      _ChangeThemeButtonWidgetState();
+}
+
+class _ChangeThemeButtonWidgetState extends State<ChangeThemeButtonWidget> {
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
-    return Switch.adaptive(
-      value: themeProvider.isDarkMode,
-      onChanged: (value) {
+    bool _isDarkMode = themeProvider.isDarkMode;
+    return IconButton(
+      icon: Icon(_isDarkMode ? Icons.light_mode : Icons.dark_mode),
+      onPressed: () {
         final provider = Provider.of<ThemeProvider>(context, listen: false);
-        provider.toggleTheme(value);
+        provider.toggleTheme(!_isDarkMode);
+        setState(() {
+          _isDarkMode = !_isDarkMode;
+        });
       },
     );
+
+    // Switch.adaptive(
+    //   value: themeProvider.isDarkMode,
+    //   onChanged: (value) {
+    //     final provider = Provider.of<ThemeProvider>(context, listen: false);
+    //     provider.toggleTheme(value);
+    //   },
+
+    // );
   }
 }
